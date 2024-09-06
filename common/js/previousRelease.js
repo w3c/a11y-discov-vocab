5927b67aa39f07ffae2b22dbc03465292ce1e92a
Matt Garrish <mattgarrish@users.noreply.github.com>
2024-09-06 15:50:32 -0400

    fix previous version script to work with any doc

--- common/js/previousRelease.js
+++ common/js/previousRelease.js
@@ -1,9 +1,6 @@
 
 function addPreviousRelease() {
 	
-	var prevLink = '<dt>Previous version:</dt>\
-<dd><a href="https://www.w3.org/community/reports/a11y-discov-vocab/CG-FINAL-vocabulary-%%ppd%%/">https://www.w3.org/community/reports/a11y-discov-vocab/CG-FINAL-vocabulary-%%ppd%%/</a></dd>';
-
 	if (!respecConfig.hasOwnProperty('previousPublishDate')) {
 		console.warn('Missing previousPublishDate in respec metadata. Cannot add link to previous release.');
 		return;
@@ -15,6 +12,9 @@ function addPreviousRelease() {
 				(respecConfig['previousPublishDate'].getMonth() + 1).toString().padStart(2,'0') +
 				(respecConfig['previousPublishDate'].getDate() + 1).toString().padStart(2,'0');
 	
+	var prevLink = '<dt>Previous version:</dt>\
+<dd><a href="https://www.w3.org/community/reports/a11y-discov-vocab/CG-FINAL-' + respecConfig['shortName'] + '-' + ppd + '/">https://www.w3.org/community/reports/a11y-discov-vocab/CG-FINAL-' + respecConfig['shortName'] + '-' + ppd + '/</a></dd>';
+
 	// get the dt for the latest editor's draft - new link will go before (nth-child not working here, so grabbing all)
 	var dt = document.querySelectorAll('div.head > dl > dt');
 	
@@ -22,8 +22,6 @@ function addPreviousRelease() {
 		console.warn('Missing dt element with publication links. Cannot add link to previous release.');
 		return;
 	}
-	
-	prevLink = prevLink.replace(/%%ppd%%/g, ppd);
 	
 	dt[1].insertAdjacentHTML('beforebegin', prevLink);
 }
